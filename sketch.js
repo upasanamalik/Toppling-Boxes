538372986a998ff46efe62e032dd90864aa2c592
upasanamalik <88521757+upasanamalik@users.noreply.github.com>
2021-09-20 19:35:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(700,300,70,70);
+    box2 = new Box(920,300,70,70);
+    ground = new Ground(600,height,1200,20)
+    log1 = new Log(810,260,300,PI/2);
+    pig1 = new Pig(810,280);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+    log1.display();
+    pig1.display();
+}
